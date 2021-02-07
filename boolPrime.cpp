0a06b9c90a7625ff8307187e20458edaeb8bd94d
RakeshPuvvala8 <62640199+RakeshPuvvala8@users.noreply.github.com>
2021-02-07 22:38:43 +0530

    Add files via upload

---
+++ boolPrime.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter A Number";
+    cin>>n;
+    bool flag=0;
+
+    for(int i=2;i<sqrt(n);i++){
+        if(n%i==0){
+            cout<<"Non-Prime"<<endl;
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        cout<<"prime"<<endl;
+    }
+}
